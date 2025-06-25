af9f0adb8fd5512fdadca408bb08c54a39cf9ef1
Micah Benac <micahbenac52@gmail.com>
2025-06-25 10:49:59 +0200

    feat: deserializer supports different bit sizes

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
-pub mod schema;
-pub mod types;
+// TODO: add null data type handling, which will be done in the following way:
+// SERIALIZATION: The schema will send an element with a value of 0 to indicate that the next element is possibly null
+//  and during serialization, the serializer will serialize a bit set depending on whether the next element's schema size
+//  also says 0.
+// DESERIALIZATION: The schema will send an element with a value of 0 to indicate that the next element is possibly null
+//  and during deserialization, there will be a bit before the possible value to indicate whether it exists or not.
 
 /// 20 subtracting one bit to ensure the character is within the valid range
 const CHAR_BIT_SPACE: usize = 19;
@@ -75,9 +79,7 @@ const CHAR_SIZE: usize = 4;
 ///     kills: number;
 /// }
 /// ```
-pub fn serialize_u8_slice_to_utf8<const N: usize>(
-    schema_data: [u8; N],
-) -> Result<String, &'static str> {
+pub fn serialize_u8_slice_to_utf8(schema_data: Vec<u8>) -> Result<String, &'static str> {
     if schema_data.is_empty() {
         return Err("Received empty schema data");
     }
@@ -86,6 +88,7 @@ pub fn serialize_u8_slice_to_utf8<const N: usize>(
     let needed_bits = BITS_IN_BYTE * schema_data.len();
     // Example: one byte should only require 1 char
     let needed_chars = needed_bits / CHAR_BIT_SPACE + ((needed_bits % CHAR_BIT_SPACE) > 0) as usize;
+    println!("needed_chars: {needed_chars}");
 
     let mut buffer: Vec<u8> = Vec::new();
     buffer.resize_with(needed_chars * 4, Default::default);
@@ -145,23 +148,48 @@ enum CharByteSegment {
 // Parses `schema_size` amount of bytes from the `buffer`
 pub fn deserialize_u8_slice_from_utf8(
     buffer: String,
-    schema_size: usize,
+    schema_bit_counts: Vec<usize>,
 ) -> Result<Vec<u8>, &'static str> {
-    let expected_bits = schema_size * BITS_IN_BYTE;
+    let expected_bits = schema_bit_counts.iter().sum::<usize>();
+    if expected_bits == 0 {
+        return Err("Received 0 sized schema");
+    }
+    if schema_bit_counts
+        .iter()
+        .any(|bit_count| ![0, 1, 2, 4, 8, 16, 32, 64].contains(bit_count))
+    {
+        return Err("Received incorrect bit count from schema");
+    }
     println!("expected_bits: {expected_bits}");
     // Example: one byte should only require 1 char
     let expected_chars =
         expected_bits / CHAR_BIT_SPACE + ((expected_bits % CHAR_BIT_SPACE) > 0) as usize;
     println!("expected_chars: {expected_chars}");
     if buffer.len() != expected_chars * CHAR_SIZE {
-        return Err("Content doesn't contain enough bytes for the schema");
+        return Err("Buffer doesn't match the expected bytes for the schema");
     }
 
+    // let schema_data_size = schema_bit_counts
+    //     .iter()
+    //     .cloned()
+    //     .reduce(|acc, e| acc + (e >= BITS_IN_BYTE).then(|| e / BITS_IN_BYTE).unwrap_or(1))
+    //     .unwrap();
+    let mut schema_expanded_bit_counts: Vec<usize> = Vec::new();
+    for bit_count in &schema_bit_counts {
+        if *bit_count > BITS_IN_BYTE {
+            for _ in 0..(bit_count / BITS_IN_BYTE) {
+                schema_expanded_bit_counts.push(BITS_IN_BYTE);
+            }
+            continue;
+        }
+        schema_expanded_bit_counts.push(*bit_count);
+    }
     let mut schema_data: Vec<u8> = Vec::new();
-    schema_data.resize_with(schema_size, Default::default);
-    let mut shift_offset = BITS_IN_BYTE - 1;
+    schema_data.resize_with(schema_expanded_bit_counts.len(), Default::default);
     let mut schema_data_iter = schema_data.iter_mut();
-    let mut data_segment = schema_data_iter.next().ok_or("Received 0 sized schema")?;
+    let mut data_segment = schema_data_iter.next().unwrap();
+    let mut schema_bit_counts = schema_expanded_bit_counts.iter();
+    let mut shift_offset = schema_bit_counts.next().unwrap() - 1;
     let mut expected_char_segment = CharByteSegment::Start;
     for char_byte in buffer.as_bytes() {
         let expected_byte_pattern: u8 = match expected_char_segment {
@@ -170,7 +198,7 @@ pub fn deserialize_u8_slice_from_utf8(
         };
 
         if char_byte & expected_byte_pattern != expected_byte_pattern {
-            return Err("Content has invalid data");
+            return Err("Buffer has invalid data");
         }
 
         let mut extract_n_iterate = |bit_count: u8| {
@@ -184,7 +212,7 @@ pub fn deserialize_u8_slice_from_utf8(
                         Some(next_segment) => data_segment = next_segment,
                         None => return false,
                     };
-                    shift_offset = BITS_IN_BYTE - 1;
+                    shift_offset = schema_bit_counts.next().unwrap() - 1;
                     continue;
                 }
                 shift_offset -= 1;
@@ -224,25 +252,29 @@ pub fn deserialize_u8_slice_from_utf8(
 
 #[cfg(test)]
 mod tests {
+    use std::mem::transmute;
+
     use super::*;
 
     #[test]
     fn serialization() {
         let value: u8 = 127;
+        // 127 serialized
         let expected_string =
             String::from_utf8(vec![0b11110010, 0b10111111, 0b10100000, 0b10000000]).unwrap();
 
         println!("bytes: {:?}", expected_string);
-        let result = serialize_u8_slice_to_utf8(value.to_le_bytes()).unwrap();
+        let result = serialize_u8_slice_to_utf8(value.to_be_bytes().to_vec()).unwrap();
         assert_eq!(result, expected_string);
     }
 
     #[test]
     fn deserialization() {
+        // 127 serialized
         let four_byte_char =
             String::from_utf8(vec![0b11110010, 0b10111111, 0b10100000, 0b10000000]).unwrap();
 
-        let result = deserialize_u8_slice_from_utf8(four_byte_char, 1);
+        let result = deserialize_u8_slice_from_utf8(four_byte_char, vec![8]);
         if let Err(message) = result {
             println!("{message}");
         }
@@ -251,22 +283,105 @@ mod tests {
 
     #[test]
     fn serialize_n_deserialize() {
-        let values = [200, 100, 50, 25, 10, 5, 1];
+        let values = [400, 200, 100, 50, 25, 10, 5, 1]
+            .map(|data: u64| {
+                if data > u8::MAX as u64 {
+                    data.to_be_bytes().to_vec()
+                } else {
+                    [data as u8].to_vec()
+                }
+            })
+            .concat();
+
+        println!(
+            "values: {:?}",
+            values
+                .iter()
+                .map(|value| format!("{value:0b}"))
+                .collect::<Vec<String>>()
+                .join(" ")
+        );
         let serialized_data = serialize_u8_slice_to_utf8(values).unwrap();
-        println!("values: {:?}", values.map(|value| format!("{value:0b}")));
         println!(
             "serialized_data: {:?}",
             serialized_data
                 .as_bytes()
                 .iter()
-                .map(|byte| format!("{byte:0b} "))
-                .collect::<String>()
+                .map(|byte| format!("{byte}"))
+                .collect::<Vec<String>>()
+                .join(" ")
         );
-        let deserialized_data = deserialize_u8_slice_from_utf8(serialized_data, values.len());
+        let deserialized_data =
+            deserialize_u8_slice_from_utf8(serialized_data, vec![64, 8, 8, 8, 8, 8, 8, 8]);
 
         if let Err(message) = deserialized_data {
             println!("{message}");
         }
+
         assert!(deserialized_data.is_ok());
+
+        println!(
+            "deserialized_data: {}",
+            deserialized_data.map_or_else(
+                |err| err.to_string(),
+                |data| [
+                    // Not a major issue but just annoying, figure out how to properly convert a slice of bytes to u64
+                    format!(
+                        "{}",
+                        u128::from_be_bytes(unsafe { transmute(data.get(0..8).unwrap()) })
+                    ),
+                    data.iter()
+                        .skip(8)
+                        .map(|byte| format!("{byte}"))
+                        .collect::<Vec<String>>()
+                        .join(" ")
+                ]
+                .join(" ")
+            )
+        );
+    }
+
+    #[test]
+    fn serialize_n_deserialize_real_sample() {
+        // Schema Id: (1 bytes)
+        let schema_id = vec![0];
+        // "Terra" (5 bytes)
+        let username = vec![0b01010100, 0b01100101, 0b01110010, 0b01110010, 0b01100001];
+        // "1071822091814441000" (19 bytes)
+        let user_id = vec![
+            0b00110001, 0b00110000, 0b00110111, 0b00110001, 0b00111000, 0b00110010, 0b00110010,
+            0b00110000, 0b00111001, 0b00110001, 0b00111000, 0b00110001, 0b00110100, 0b00110100,
+            0b00110100, 0b00110001, 0b00110000, 0b00110000, 0b00110000,
+        ];
+
+        let schema_data = [schema_id, username, user_id].concat();
+
+        let serialized_data = serialize_u8_slice_to_utf8(schema_data).unwrap();
+
+        println!("serialized_data: {}", serialized_data);
+
+        let deserialized_data = deserialize_u8_slice_from_utf8(
+            serialized_data,
+            [vec![8], [8].repeat(5), [8].repeat(19)].concat(),
+        )
+        .unwrap();
+        let mut deserialized_data_iter = deserialized_data.iter();
+
+        let schema_id = deserialized_data_iter.next().unwrap();
+        println!("{schema_id}");
+
+        let mut username_parts = Vec::new();
+        for _ in 0..5 {
+            username_parts.push(*deserialized_data_iter.next().unwrap());
+        }
+        let username = String::from_utf8(username_parts).unwrap();
+        println!("{username}");
+
+        let mut user_id_parts = Vec::new();
+        for _ in 0..19 {
+            user_id_parts.push(*deserialized_data_iter.next().unwrap());
+        }
+        let user_id = String::from_utf8(user_id_parts).unwrap();
+        println!("{user_id}");
     }
 }
